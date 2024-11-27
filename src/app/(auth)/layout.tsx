e8f74ad842a7ee519086597933f685702b0a0521
jonathancaleb <jonathancaleb230@gmail.com>
2024-11-27 17:07:48 +0300

    updates

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+
+import Image from "next/image";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <div>
+        <Image src="/logo.svg" alt="logo" height={20} width={50} />
+        {children}</div>
+  );
+};
+
+export default AuthLayout;
